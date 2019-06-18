8f801aea888430a9e661b0eaffeb0a332f95b455
Fehér Ágnes <feher.agnes@ENCOSOFT.INTERNAL>
2019-06-18 13:52:46 +0200

    background and font

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './features/login/login/login.component';
+import { RegisterComponent } from './features/login/register/register.component';
 
 const routes: Routes = [
   {
     path: 'battle',
     loadChildren: './features/battle/battle.module#BattleModule'
-  }
+  },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: '',   redirectTo: '/login', pathMatch: 'full' },
 ];
 
 @NgModule({
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,14 +5,52 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {
+  MatButtonModule,
+  MatCardModule,
+  MatChipsModule,
+  MatExpansionModule,
+  MatFormFieldModule,
+  MatIconModule,
+  MatInputModule,
+  MatListModule,
+  MatProgressSpinnerModule,
+  MatSelectModule,
+  MatSidenavModule,
+  MatSliderModule,
+  MatToolbarModule,
+  MatGridListModule
+} from '@angular/material';
+
+import { LoginComponent } from './features/login/login/login.component';
+import { RegisterComponent } from './features/login/register/register.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    CoreModule
+    CoreModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatChipsModule,
+  MatExpansionModule,
+  MatFormFieldModule,
+  MatIconModule,
+  MatInputModule,
+  MatListModule,
+  MatProgressSpinnerModule,
+  MatSelectModule,
+  MatSidenavModule,
+  MatSliderModule,
+  MatToolbarModule,
+  MatGridListModule,
+  MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
