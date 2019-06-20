2d7414f1b7d2d670d13b88b53d1fab0ee522193d
Richárd Pasics <43377943+Wolvarr@users.noreply.github.com>
2019-06-20 16:37:39 +0200

    Egyseg controller

--- backend/StrategyGame.Api/Controllers/EgysegController.cs
+++ backend/StrategyGame.Api/Controllers/EgysegController.cs
@@ -2,23 +2,31 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using StrategyGame.Bll.DTOs;
+using StrategyGame.Bll.DTOs.Egysegek;
 
 namespace StrategyGame.Api.Controllers
 {
     [Route("[controller]/[action]")]
     [ApiController]
-    public class EpuletController : Controller
+    public class EgysegController : ControllerBase
     {
         [HttpGet]
-        public async Task<IActionResult> GetUserEpulets()
+        public async Task<IActionResult> GetUserEgysegs()
+        {
+            return Ok("Not implemented");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetEgysegInfos()
         {
             return Ok("Not implemented");
         }
 
         [HttpPost]
-        public async Task<IActionResult> BuyEpulets([FromBody] EpuletInfoDTO epulet)
+        public async Task<IActionResult> PostBuyEgysegs([FromBody] SeregInfoDTO egysegs)
         {
             return Ok("Not implemented");
         }
---
+++ backend/StrategyGame.Api/Controllers/FejlesztesController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using StrategyGame.Bll.DTOs.Fejlesztesek;
+
+namespace StrategyGame.Api.Controllers
+{
+    [Route("[controller]/[action]")]
+    [ApiController]
+    public class FejlesztesController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetAllFejleszteses()
+        {
+            return Ok("Not implemented");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetFejlesztesInfos()
+        {
+            return Ok("Not implemented");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> BuyFejlesztes([FromBody] FejlesztesDTO epulet)
+        {
+            return Ok("Not implemented");
+        }
+
+    }
+}
