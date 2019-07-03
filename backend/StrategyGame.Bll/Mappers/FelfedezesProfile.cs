ee63ee2c7d58a5817fc087f593d0d190830e8cb0
Richárd Pasics <43377943+Wolvarr@users.noreply.github.com>
2019-07-03 13:42:35 +0200

    Felfedezes mapper added

---
+++ backend/StrategyGame.Bll/Mappers/FelfedezesProfile.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using StrategyGame.Bll.DTOs;
+using StrategyGame.Model.Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StrategyGame.Bll.Mappers
+{
+    class FelfedezesProfile : Profile
+    {
+        public FelfedezesProfile()
+        {
+            CreateMap<FelfedezesDTO, Felfedezes>()
+                 .ForMember(e => e.Celpont, opt => opt.MapFrom(e => e.VedekezoOrszag))
+                 .ForMember(e => e.VedekezoEro, opt => opt.MapFrom(e => e.VedekezoEro))
+                 .ForMember(e => e.Gyongy, opt => opt.MapFrom(e => e.VedekezoGyongy))
+                 .ForMember(e => e.Korall, opt => opt.MapFrom(e => e.VedekezoKorall))
+                .ReverseMap();
+        }
+    }
+}
+
